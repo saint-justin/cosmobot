67ee36dd63ca232d247fbc176f9f4d73043d0d89
Justin Vaughn <gearphreak@gmail.com>
2023-12-29 09:07:09 -0800

    feat: Initial commit, basic bot outline + aws helper for token management

---
+++ src/aws_helpers.rs
@@ -0,0 +1,18 @@
+use aws_config::meta::region::RegionProviderChain;
+use aws_config::BehaviorVersion;
+use aws_sdk_secretsmanager::{Error, Client};
+const SECRET_ID: &str = "bot_token";
+
+/// Retreives the Discord bot API token from AWS SecretsManager, returns
+/// a result or any error that prevented token retrieval
+pub async fn get_bot_token() -> Result<String, Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::defaults(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
+
+    let client = Client::new(&config);
+    let res = client.get_secret_value().secret_id(SECRET_ID).send().await?;
+    Ok(res.secret_string().unwrap().to_owned())
+}
